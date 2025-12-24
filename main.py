90cb9a6f0d90a3bae11061a5ad663528eb753f21
Natcha Phonkamhaeng <natchaphonkamhaeng@192.168.1.7>
2025-12-24 09:18:15 +0700

    version2 keeping score

--- main.py
+++ main.py
@@ -20,16 +20,17 @@ def game_logic():
             print("YOU LOSE!!")
             break
 
-        user_guess = input("enter your guess words: ")
+        user_guess = input("enter your guess letter: ")
+        guess.append(user_guess)
 
-        if com == user_guess:
-            print("that's correct!")
-            break
+        if user_guess in split_text:
+            print("that's is correct")
+            print(guess)
         else:
-            print("Incorrect Guess Again!")
+            print("Nope Try another letter!!")
             print(hma.stages[stages])
-            print(f"number of guesses left {stages}")
             stages -= 1
+
 
 def main():
     print(split_text)
@@ -38,16 +39,25 @@ def main():
 
     print(hma.logo)
 
-    user_guess = input("enter your guess letter: ")
-    guess.append(user_guess)
+    while True:
 
-    if user_guess in split_text:
-        print("that's is correct")
-        print(guess)
-    else:
-        print("Nope Try another letter!!")
-        print(hma.stages[stages])
-        stages -= 1
+        if stages < 0:
+            print("YOU LOSE!!")
+            break
+
+        user_guess = input("enter your guess letter: ")
+        guess.append(user_guess)
+
+        if user_guess in split_text:
+            print("that's is correct")
+            print(guess)
+        elif random_words == user_guess:
+            print("you won")
+            break
+        else:
+            print("Nope Try another letter!!")
+            print(hma.stages[stages])
+            stages -= 1
 
      
 if __name__ == "__main__":
