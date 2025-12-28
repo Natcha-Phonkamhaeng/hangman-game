ffad457fa0b9e3060e8479b2aa996a00dd04b752
Natcha Phonkamhaeng <natchaphonkamhaeng@192.168.1.14>
2025-12-28 08:07:33 +0700

    version 2 guessing by alphbet

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
 from asset import hangman_art as hma
+from asset import hangman_word as hmw
 import random
 
-# 13 Dec 2025, hangman guessing game V1 guessing the whole word with hint
-
-words = [
-    "python", "hangman", "programming", "developer",
-    "computer", "algorithm", "function", "variable"
-]
-
-random_words = random.choice(words)
+random_words = random.choice(hmw.word_list)
 split_text = " ".join(random_words)
 com = random_words
-
-def game_logic():
-
-     while True:
-
-        if stages < 0:
-            print("YOU LOSE!!")
-            break
-
-        user_guess = input("enter your guess letter: ")
-        guess.append(user_guess)
-
-        if user_guess in split_text:
-            print("that's is correct")
-            print(guess)
-        else:
-            print("Nope Try another letter!!")
-            print(hma.stages[stages])
-            stages -= 1
-
 
 def main():
     print(split_text)
@@ -46,9 +20,9 @@ def main():
             break
 
         user_guess = input("enter your guess letter: ")
-        guess.append(user_guess)
 
         if user_guess in split_text:
+            guess.append(user_guess)
             print("that's is correct")
             print(guess)
         elif random_words == user_guess:
@@ -58,7 +32,6 @@ def main():
             print("Nope Try another letter!!")
             print(hma.stages[stages])
             stages -= 1
-
-     
+    
 if __name__ == "__main__":
     main()
